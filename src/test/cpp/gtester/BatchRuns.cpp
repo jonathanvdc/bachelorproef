9550ae8c011770cda6ce933fa85360bbbc45b75c
Jan Broeckhove <Jan.Broeckhove@uantwerpen.be>
2016-12-27 16:47:20 +0100

    Cleaned up test infrastructure.

--- src/test/cpp/gtester/BatchRuns.cpp
+++ src/test/cpp/gtester/BatchRuns.cpp
@@ -15,7 +15,7 @@
 
 /**
  * @file
- * Implementation of scenario test running demo cases in batch mode.
+ * Implementation of scenario tests running in batch mode.
  */
 
 #include "core/Population.h"
@@ -26,10 +26,9 @@
 #include <omp.h>
 
 #include <cmath>
-#include <iostream>
-#include <fstream>
 #include <map>
 #include <string>
+#include <tuple>
 
 using namespace std;
 using namespace stride;
@@ -37,7 +36,7 @@ using namespace ::testing;
 
 namespace Tests {
 
-class BatchDemos: public TestWithParam<string>
+class BatchDemos: public ::testing::TestWithParam<tuple<string, unsigned int>>
 {
 public:
 	/// TestCase set up.
@@ -72,11 +71,7 @@ protected:
 	static const double            g_transmission_rate_adapted;
 	static const string            g_disease_config_file_adapted;
 
-
 	static const map<string, unsigned int>   g_results;
-#ifdef _OPENMP
-	static const map<string, unsigned int>   g_results_omp;
-#endif
 };
 
 // Default values
@@ -95,72 +90,7 @@ const double         BatchDemos::g_immunity_rate_adapted       = 0.1;
 const double         BatchDemos::g_transmission_rate_adapted   = 0.000035;
 const string         BatchDemos::g_disease_config_file_adapted = "disease_config_measles.xml";
 
-/// map<tranmsmission rate, number of cases>
 const map<string, unsigned int> BatchDemos::g_results {
-	make_pair("default",120822),
-	make_pair("rng_seed",29),
-	make_pair("seeding_rate",124047),
-	make_pair("immunity_rate",87474),
-	make_pair("transmission_rate",176324),
-	make_pair("measles",60398)
-};
-
-TEST_P( BatchDemos, RunOnce )
-{
-#ifdef _OPENMP
-	// set nb of threads to 1 to reproduce serial execution results
-	omp_set_num_threads(1);
-	omp_set_schedule(omp_sched_dynamic,1);
-#endif
-
-	// Prepare the configuration
-        const string test_tag = GetParam();
-	const string disease_config_file = (test_tag != "measles") ? g_disease_config_file : g_disease_config_file_adapted;
-
-	boost::property_tree::ptree pt_config;
-	pt_config.put("run.r0", g_r0);
-	pt_config.put("run.population_file", g_population_file);
-	pt_config.put("run.transmission_rate",g_transmission_rate);
-	pt_config.put("run.num_days", g_num_days);
-	pt_config.put("run.rng_seed", g_rng_seed);
-	pt_config.put("run.seeding_rate", g_seeding_rate);
-	pt_config.put("run.immunity_rate", g_immunity_rate);
-	pt_config.put("run.disease_config_file", disease_config_file);
-
-	// override scenario setting
-	if (test_tag == "default"){
-		// do nothing
-	}
-	if (test_tag == "rng_seed"){
-		pt_config.put("run.rng_seed", g_rng_seed_adapted);
-	}
-	if (test_tag == "seeding_rate"){
-		pt_config.put("run.seeding_rate", g_seeding_rate_adapted);
-	}
-	if (test_tag == "immunity_rate"){
-		pt_config.put("run.immunity_rate", g_immunity_rate_adapted);
-	}
-	if (test_tag == "transmission_rate"){
-		pt_config.put("run.transmission_rate", g_transmission_rate_adapted);
-	}
-
-	// Initialize simulator and run it.
-	Simulator sim(pt_config);
-	cerr << "  starting up: " << test_tag << endl;
-	for(size_t i = 0U; i < g_num_days; i++){
-		sim.UpdateTimeStep();
-	}
-
-	// Round up
-	const unsigned int num_cases = sim.GetPopulation()->GetInfectedCount();
-	ASSERT_EQ(num_cases, g_results.at(test_tag)) << "!! CHANGED !!";
-	cerr << "  finished:    " << test_tag << endl;
-}
-
-
-#ifdef _OPENMP
-
-const map<string, unsigned int> BatchDemos::g_results_omp {
 	make_pair("default", 125099),
 	make_pair("rng_seed",111392),
 	make_pair("seeding_rate",127143),
@@ -169,15 +99,17 @@ const map<string, unsigned int> BatchDemos::g_results_omp {
 	make_pair("measles",123104)
 };
 
-TEST_P( BatchDemos, RunOnce_omp )
+TEST_P( BatchDemos, Run )
 {
-	// set nb of threads to 4 and use static scheduling to obtain repeatable results
-	omp_set_num_threads(4);
-	omp_set_schedule(omp_sched_static,1);
-
-        // Prepare the configuration
-        const string test_tag = GetParam();
+        // Prepare test configuration.
+        tuple<string, unsigned int> t(GetParam());
+        const string test_tag = get<0>(t);
 	const string disease_config_file = (test_tag != "measles") ? g_disease_config_file : g_disease_config_file_adapted;
+
+	// Prepare test configuration
+	const unsigned int num_threads = get<1>(t);
+        omp_set_num_threads(num_threads);
+        omp_set_schedule(omp_sched_static,1);
 
 	// create pt_config
 	boost::property_tree::ptree pt_config;
@@ -206,20 +138,16 @@ TEST_P( BatchDemos, RunOnce_omp )
 		pt_config.put("run.transmission_rate", g_transmission_rate_adapted);
 	}
 
-
 	// Initialize simulator and run it.
 	Simulator sim(pt_config);
-	cerr << "  starting up ..." << endl;
-	for (size_t i = 0U; i < g_num_days; i++){
+	for (size_t i = 0U; i < g_num_days; i++) {
 		sim.UpdateTimeStep();
 	}
 
 	// Round up
 	const unsigned int num_cases = sim.GetPopulation()->GetInfectedCount();
-	ASSERT_EQ(num_cases, g_results_omp.at(test_tag)) << "!! CHANGED !!";
-	cerr << "  finished:    " << test_tag << endl;
+	ASSERT_EQ(num_cases, g_results.at(test_tag)) << "!! CHANGED !!";
 }
-#endif
 
 namespace {
 	string scenarios[] {
@@ -230,9 +158,17 @@ namespace {
 		"transmission_rate",
 		"measles"
 	};
+
+#ifdef _OPENMP
+	unsigned int threads[] { 1U, 2U, 4U, 8U };
+#else
+	unsigned int threads[] { 1U };
+#endif
+
 }
 
-INSTANTIATE_TEST_CASE_P(RunBatch, BatchDemos, ValuesIn(scenarios));
+INSTANTIATE_TEST_CASE_P(RunBatch, BatchDemos,
+        ::testing::Combine(::testing::ValuesIn(scenarios), ::testing::ValuesIn(threads)));
 
 } //end-of-namespace-Tests
 
