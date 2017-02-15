c65e086166bd45807bc2c6e15e2e5a6034c52c78
lwillem <lander.willem@uantwerpen.be>
2017-02-15 13:33:59 +0100

    Adjusted gtester to current Simulator design and input with 4 basic tests. Minor update of config files with new population files.

--- src/test/cpp/gtester/BatchRuns.cpp
+++ src/test/cpp/gtester/BatchRuns.cpp
@@ -25,6 +25,7 @@
 #include <gtest/gtest.h>
 #include <boost/property_tree/ptree.hpp>
 #include <omp.h>
+#include <spdlog/spdlog.h>
 
 #include <cmath>
 #include <map>
@@ -63,105 +64,155 @@ protected:
 	static const unsigned int      g_rng_seed;
 	static const double            g_seeding_rate;
 	static const double            g_immunity_rate;
-	static const double            g_transmission_rate;
 	static const string            g_disease_config_file;
+	static const string            g_output_prefix;
+	static const string            g_holidays_file;
+	static const unsigned int      g_num_participants_survey;
+	static const string            g_start_date;
 
 	static const unsigned int      g_rng_seed_adapted;
 	static const double            g_seeding_rate_adapted;
 	static const double            g_immunity_rate_adapted;
-	static const double            g_transmission_rate_adapted;
 	static const string            g_disease_config_file_adapted;
+	static const double            g_transmission_rate_measles;
+	static const double            g_transmission_rate_maximum;
 
 	static const map<string, unsigned int>   g_results;
 };
 
 // Default values
-const string         BatchDemos::g_population_file             = "nassau_synt_pop_sorted.csv";
-const double         BatchDemos::g_r0                          = 1.1;
-const unsigned int   BatchDemos::g_num_days                    = 100U;
+const string         BatchDemos::g_population_file             = "pop_oklahoma.csv";
+const double         BatchDemos::g_r0                          = 3.0;
+const unsigned int   BatchDemos::g_num_days                    = 30U;
 const unsigned int   BatchDemos::g_rng_seed                    = 2015U;
-const double         BatchDemos::g_seeding_rate                = 0.00001;
+const double         BatchDemos::g_seeding_rate                = 0.0001;
 const double         BatchDemos::g_immunity_rate               = 0.0;
-const double         BatchDemos::g_transmission_rate           = 0.00003;
-const string         BatchDemos::g_disease_config_file         = "disease_config_influenza.xml";
+const string         BatchDemos::g_disease_config_file         = "disease_influenza.xml";
+const string         BatchDemos::g_output_prefix         	   = "test";
+const string         BatchDemos::g_holidays_file               = "holidays_none.json";
+const unsigned int   BatchDemos::g_num_participants_survey     = 10;
+const string         BatchDemos::g_start_date                  = "2017-01-01";
 
-const unsigned int   BatchDemos::g_rng_seed_adapted            = 1U;
-const double         BatchDemos::g_seeding_rate_adapted        = 0.00002;
-const double         BatchDemos::g_immunity_rate_adapted       = 0.1;
-const double         BatchDemos::g_transmission_rate_adapted   = 0.000035;
-const string         BatchDemos::g_disease_config_file_adapted = "disease_config_measles.xml";
+// Adapted values
+const double         BatchDemos::g_seeding_rate_adapted        = 0.0;
+const double         BatchDemos::g_immunity_rate_adapted       = 0.999991;
+const string         BatchDemos::g_disease_config_file_adapted = "disease_measles.xml";
+const double		 BatchDemos::g_transmission_rate_measles   = 16U;
+const double   		 BatchDemos::g_transmission_rate_maximum   = 100U;
 
 const map<string, unsigned int> BatchDemos::g_results {
-	make_pair("default", 125099),
-	make_pair("rng_seed",111392),
-	make_pair("seeding_rate",127143),
-	make_pair("immunity_rate",87034),
-	make_pair("transmission_rate",181001),
-	make_pair("measles",123104)
+	make_pair("default", 70000),
+	make_pair("seeding_rate",0),
+	make_pair("immunity_rate",6),
+	make_pair("measles",135000),
+	make_pair("maximum",700000)
 };
 
 TEST_P( BatchDemos, Run )
 {
-        // Prepare test configuration.
-        tuple<string, unsigned int> t(GetParam());
-        const string test_tag = get<0>(t);
-	const string disease_config_file = (test_tag != "measles") ? g_disease_config_file : g_disease_config_file_adapted;
-
-	// Prepare test configuration
+	// -----------------------------------------------------------------------------------------
+	// Prepare test configuration.
+	// -----------------------------------------------------------------------------------------
+	tuple<string, unsigned int> t(GetParam());
+	const string test_tag = get<0>(t);
 	const unsigned int num_threads = get<1>(t);
-        omp_set_num_threads(num_threads);
-        omp_set_schedule(omp_sched_static,1);
+	omp_set_num_threads(num_threads);
+	omp_set_schedule(omp_sched_static,1);
 
-	// create pt_config
+	// -----------------------------------------------------------------------------------------
+	// Setup configuration.
+	// -----------------------------------------------------------------------------------------
 	boost::property_tree::ptree pt_config;
-	pt_config.put("run.population_file", g_population_file);
-	pt_config.put("run.transmission_rate",g_transmission_rate);
-	pt_config.put("run.num_days", g_num_days);
 	pt_config.put("run.rng_seed", g_rng_seed);
+	pt_config.put("run.r0", g_r0);
 	pt_config.put("run.seeding_rate", g_seeding_rate);
 	pt_config.put("run.immunity_rate", g_immunity_rate);
-	pt_config.put("run.disease_config_file", disease_config_file);
+	pt_config.put("run.population_file", g_population_file);
+	pt_config.put("run.num_days", g_num_days);
+	pt_config.put("run.output_prefix", g_output_prefix);
+	pt_config.put("run.disease_config_file",g_disease_config_file);
+	pt_config.put("run.num_participants_survey",g_num_participants_survey);
+	pt_config.put("run.start_date",g_start_date);
+	pt_config.put("run.holidays_file",g_holidays_file);
+	pt_config.put("run.age_contact_matrix_file","contact_matrix_average.xml");
+	pt_config.put("run.log_level","None");
 
-	// override scenario setting
+	bool track_index_case = false;
+
+
+	// -----------------------------------------------------------------------------------------
+	// Override scenario settings.
+	// -----------------------------------------------------------------------------------------
 	if (test_tag == "default"){
 		// do nothing
-	}
-	if (test_tag == "rng_seed"){
-		pt_config.put("run.rng_seed", g_rng_seed_adapted);
 	}
 	if (test_tag == "seeding_rate"){
 		pt_config.put("run.seeding_rate", g_seeding_rate_adapted);
 	}
 	if (test_tag == "immunity_rate"){
+		pt_config.put("run.seeding_rate", 1-g_immunity_rate_adapted);
 		pt_config.put("run.immunity_rate", g_immunity_rate_adapted);
 	}
-	if (test_tag == "transmission_rate"){
-		pt_config.put("run.transmission_rate", g_transmission_rate_adapted);
+	if (test_tag == "measles"){
+		pt_config.put("run.disease_config_file", g_disease_config_file_adapted);
+		pt_config.put("run.r0", g_transmission_rate_measles);
+	}
+	if (test_tag == "maximum"){
+		pt_config.put("run.r0", g_transmission_rate_maximum);
 	}
 
-	// Initialize simulator and run it.
-	auto sim = SimulatorBuilder::Build(pt_config, num_threads);
-	for (size_t i = 0U; i < g_num_days; i++) {
-		sim->TimeStep();
+
+	// -----------------------------------------------------------------------------------------
+	// Initialize the logger.
+	// -----------------------------------------------------------------------------------------
+	spdlog::set_async_mode(1048576);
+	auto file_logger = spdlog::rotating_logger_mt("contact_logger", g_output_prefix + "_logfile",
+			std::numeric_limits<size_t>::max(),  std::numeric_limits<size_t>::max());
+	file_logger->set_pattern("%v"); // Remove meta data from log => time-stamp of logging
+
+
+	// -----------------------------------------------------------------------------------------
+	// Initialize the simulation.
+	// -----------------------------------------------------------------------------------------
+	cout << "Building the simulator. "<< endl;
+	auto sim = SimulatorBuilder::Build(pt_config, num_threads, track_index_case);
+	cout << "Done building the simulator. "<< endl <<endl;
+
+
+	// -----------------------------------------------------------------------------------------
+	// Run the simulation.
+	// -----------------------------------------------------------------------------------------
+	const unsigned int num_days = pt_config.get<unsigned int>("run.num_days");
+	for (unsigned int i = 0; i < num_days; i++) {
+			sim->TimeStep();
 	}
 
-	// Round up
+
+	// -----------------------------------------------------------------------------------------
+	// Release and close logger.
+	// -----------------------------------------------------------------------------------------
+      spdlog::drop_all();
+
+
+    // -----------------------------------------------------------------------------------------
+	// Round up.
+    // -----------------------------------------------------------------------------------------
 	const unsigned int num_cases = sim->GetPopulation()->GetInfectedCount();
-	ASSERT_EQ(num_cases, g_results.at(test_tag)) << "!! CHANGED !!";
+	ASSERT_NEAR(num_cases, g_results.at(test_tag),10000) << "!! CHANGED !!";
+
 }
 
 namespace {
 	string scenarios[] {
 		"default",
-		"rng_seed",
 		"seeding_rate",
 		"immunity_rate",
-		"transmission_rate",
-		"measles"
+		"measles",
+		"maximum"
 	};
 
 #ifdef _OPENMP
-	unsigned int threads[] { 1U, 2U, 4U, 8U };
+	unsigned int threads[] { 1U , 4U, 8U};
 #else
 	unsigned int threads[] { 1U };
 #endif
