237cc4d10534d78e5f49962a12f78065e1e4fedc
Lynn <lynn@foldr.moe>
2017-03-14 18:13:38 +0100

    clang-format Person.h

--- src/main/cpp/pop/Person.h
+++ src/main/cpp/pop/Person.h
@@ -39,14 +39,15 @@ class Person
 {
 public:
 	/// Constructor: set the person data.
-	Person(unsigned int id, double age, unsigned int household_id, unsigned int school_id,
-			unsigned int work_id, unsigned int primary_community_id, unsigned int secondary_community_id, disease::Fate fate)
-		: m_id(id), m_age(age), m_gender('M'),
-		  m_household_id(household_id), m_school_id(school_id),
-		  m_work_id(work_id), m_primary_community_id(primary_community_id), m_secondary_community_id(secondary_community_id),
-		  m_at_household(true), m_at_school(true),m_at_work(true),m_at_primary_community(true), m_at_secondary_community(true),
-		  m_health(fate),
-		  m_is_participant(false) {}
+	Person(unsigned int id, double age, unsigned int household_id, unsigned int school_id, unsigned int work_id,
+	       unsigned int primary_community_id, unsigned int secondary_community_id, disease::Fate fate)
+	    : m_id(id), m_age(age), m_gender('M'), m_household_id(household_id), m_school_id(school_id),
+	      m_work_id(work_id), m_primary_community_id(primary_community_id),
+	      m_secondary_community_id(secondary_community_id), m_at_household(true), m_at_school(true),
+	      m_at_work(true), m_at_primary_community(true), m_at_secondary_community(true), m_health(fate),
+	      m_is_participant(false)
+	{
+	}
 
 	/// Is this person not equal to the given person?
 	bool operator!=(const Person& p) const { return p.m_id != m_id; }
@@ -57,20 +58,20 @@ public:
 	/// Get cluster ID of cluster_type
 	unsigned int GetClusterId(ClusterType cluster_type) const;
 
-    /// Return person's gender.
+	/// Return person's gender.
 	char GetGender() const { return m_gender; }
 
-  	/// Return person's health status.
-	Health& GetHealth()  { return m_health; }
+	/// Return person's health status.
+	Health& GetHealth() { return m_health; }
 
 	/// Return person's health status.
 	const Health& GetHealth() const { return m_health; }
 
 	/// Get the id.
-        unsigned int GetId() const { return m_id; }
+	unsigned int GetId() const { return m_id; }
 
-    /// Check if a person is present today in a given cluster
-    bool IsInCluster(ClusterType c) const;
+	/// Check if a person is present today in a given cluster
+	bool IsInCluster(ClusterType c) const;
 
 	/// Does this person participates in the social contact study?
 	bool IsParticipatingInSurvey() const { return m_is_participant; }
@@ -82,25 +83,29 @@ public:
 	void Update(bool is_work_off, bool is_school_off);
 
 private:
-	unsigned int    m_id;                     ///< The id.
-	double          m_age;                    ///< The age.
-	char            m_gender;                 ///< The gender.
+	unsigned int m_id;
+	double m_age;
+	char m_gender;
 
-	unsigned int    m_household_id;           ///< The household id.
-	unsigned int    m_school_id;              ///< The school cluster id
-	unsigned int    m_work_id;                ///< The work cluster id
-	unsigned int    m_primary_community_id;   ///< The primary community id
-	unsigned int    m_secondary_community_id; ///< The secondary community id
+	/// Which communities does this person belong to?
+	unsigned int m_household_id;
+	unsigned int m_school_id;
+	unsigned int m_work_id;
+	unsigned int m_primary_community_id;
+	unsigned int m_secondary_community_id;
 
-	bool            m_at_household;           ///< Is person present at household today?
-	bool            m_at_school;              ///< Is person present at school today?
-	bool            m_at_work;                ///< Is person present at work today?
-	bool            m_at_primary_community;   ///< Is person present at primary_community today?
-	bool            m_at_secondary_community;  ///< Is person present at secundary_community today?
+	/// Which of those communities are they present at today?
+	bool m_at_household;
+	bool m_at_school;
+	bool m_at_work;
+	bool m_at_primary_community;
+	bool m_at_secondary_community;
 
-	Health          m_health;                ///< Health info for this person.
+	/// Health info for this person.
+	Health m_health;
 
-	bool            m_is_participant;        ///< Is participating in the social contact study
+	/// Is this person participating in the social contact study?
+	bool m_is_participant;
 };
 
 } // end_of_namespace
