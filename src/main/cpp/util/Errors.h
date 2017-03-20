3535d140adf1a6e655dcd2fc511921bf3acf527a
jonathanvdc <jonathan.vdc@outlook.com>
2017-03-20 16:40:17 +0100

    Format Errors.h

--- src/main/cpp/util/Errors.h
+++ src/main/cpp/util/Errors.h
@@ -1,10 +1,9 @@
 #ifndef STRIDE_ERRORS_H_INCLUDED
 #define STRIDE_ERRORS_H_INCLUDED
 
-#include <string>
 #include <exception>
+#include <string>
 
-#define FATAL_ERROR(message) \
-throw runtime_error(std::string(__func__)  + "> " + (message))
+#define FATAL_ERROR(message) throw runtime_error(std::string(__func__) + "> " + (message))
 
 #endif // end-of-include-guard
