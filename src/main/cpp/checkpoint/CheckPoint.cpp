75a0753b8a85d0588932bd7f025b25fc3d5f7925
cekefun <cekefun@gmail.com>
2017-04-05 16:04:31 +0200

    Old checkpoint

--- src/main/cpp/checkpoint/CheckPoint.cpp
+++ src/main/cpp/checkpoint/CheckPoint.cpp
@@ -8,7 +8,6 @@
 #include "CheckPoint.h"
 #include "hdf5.h"
 
-
 #include <sstream>
 #include <string>
 
@@ -27,6 +26,8 @@ CheckPoint::CheckPoint(std::string filename, bool create_mode)
 void CheckPoint::CreateFile(std::string filename)
 {
 	m_file = H5Fcreate(filename.c_str(), H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+	hid_t group = H5Gcreate2(m_file, "Disease", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+	H5Gclose(group);
 	// TODO: add a lot of preknown data such as Holidays, info about disease,...
 }
 
@@ -39,8 +40,16 @@ void CheckPoint::WriteDate(const Calendar& cal)
 	std::size_t year = cal.GetYear();
 
 	std::stringstream ss;
-	ss << year << '/' << month << '/' <<day;
-	std::string date = ss.str();
+	ss << year;
+	if (month < 10) {
+		ss << 0;
+	}
+	ss << month;
+	if (day < 10) {
+		ss << 0;
+	}
+	ss << day;
+	unsigned int date = std::stoi(ss.str());
 	// TODO: write as attribute to dataset
 }
 
@@ -69,7 +78,93 @@ void CheckPoint::WriteHolidays(const boost::property_tree::ptree& holidays_ptree
 			school_holidays.push_back(new_holiday);
 		}
 	}
-	// TODO: write the holidays to the checkpoint file
+	unsigned int school_dset[school_holidays.size()];
+	unsigned int holi_dset[holidays.size()];
+
+	for (unsigned int i = 0; i < holidays.size(); i++) {
+		holi_dset[i] = std::stoi(to_iso_string(holidays[i]));
+	}
+
+	for (unsigned int i = 0; i < school_holidays.size(); i++) {
+		school_dset[i] = std::stoi(to_iso_string(school_holidays[i]));
+	}
+
+	// write the general holidays
+	hsize_t dims[1] = {holidays.size() - 1};
+	hid_t dataspace = H5Screate_simple(1, dims, NULL);
+	hid_t dataset = H5Dcreate2(m_file, "holidays", H5T_STD_I32BE, dataspace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+	H5Dwrite(dataset, H5T_NATIVE_UINT, H5S_ALL, H5S_ALL, H5P_DEFAULT, holi_dset);
+	H5Sclose(dataspace);
+	H5Dclose(dataset);
+
+	// write the school holidays
+	dims[1] = school_holidays.size() - 1;
+	dataspace = H5Screate_simple(1, dims, NULL);
+	dataset =
+	    H5Dcreate2(m_file, "school_holidays", H5T_STD_I32BE, dataspace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+	H5Dwrite(dataset, H5T_NATIVE_UINT, H5S_ALL, H5S_ALL, H5P_DEFAULT, school_dset);
+	H5Sclose(dataspace);
+	H5Dclose(dataset);
+}
+
+void CheckPoint::WriteDisease(const boost::property_tree::ptree& ptree)
+{
+	hid_t group = H5Gcreate2(m_file, "Disease", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+	std::vector<std::string> fields = {"start_infectiousness", "start_symptomatic", "time_infectious",
+					   "time_symptomatic"};
+
+	for (auto field : fields) {
+		boost::property_tree::ptree pt_probability_list = ptree.get_child("disease." + field);
+		std::vector<double> probabilities;
+
+		for (const auto& i : pt_probability_list) {
+			probabilities.push_back(i.second.get_value<double>());
+		}
+
+		double dset[probabilities.size() - 1];
+		std::copy(probabilities.begin(), probabilities.end(), dset);
+		hsize_t dims[1] = {probabilities.size() - 1};
+		hid_t dataspace = H5Screate_simple(1, dims, NULL);
+		hid_t dataset =
+		    H5Dcreate2(group, field.c_str(), H5T_STD_I32BE, dataspace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+		H5Dwrite(dataset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, dset);
+		H5Dclose(dataset);
+		H5Sclose(dataspace);
+	}
+	H5Gclose(group);
+}
+
+void CheckPoint::WritePopulation(const Population& pop)
+{
+	unsigned int dset[pop.size() - 1][5 + NumOfClusterTypes() - 1];
+	for (unsigned int i = 0; i < pop.size(); i++) {
+		Person p = pop[i];
+
+		// Basic Data
+		dset[i][0] = p.GetId();
+		dset[i][1] = p.GetAge();
+		dset[i][2] = p.GetGender();
+		dset[i][3] = p.IsParticipatingInSurvey();
+
+		// Health data
+		dset[i][4] = p.GetHealth().GetHealthStatus();
+
+		// Cluster data
+		for (unsigned int j = 0; j < NumOfClusterTypes(); j++) {
+			if (ToString(j) == "Null") {
+				continue;
+			}
+			dset[i][5 + j] = p.GetClusterId(j);
+		}
+	}
+	hsize_t dims[2] = {pop.size() - 1, 5 + NumOfClusterTypes() - 1};
+	hid_t dataspace = H5Screate_simple(2, dims, NULL);
+	// TODO : Get name to  be the date
+	hid_t dataset =
+	    H5Dcreate2(m_file, "Population/", H5T_STD_I32BE, dataspace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+	H5Dwrite(dataset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, dset);
+	H5Dclose(dataset);
+	H5Sclose(dataspace);
 }
 
 } /* namespace checkpoint */
--- src/main/cpp/checkpoint/CheckPoint.h
+++ src/main/cpp/checkpoint/CheckPoint.h
@@ -8,8 +8,10 @@
 #ifndef CHECKPOINT_H_INCLUDED
 #define CHECKPOINT_H_INCLUDED
 
+#include <memory>
 #include "calendar/Calendar.h"
 #include "hdf5.h"
+#include "pop/Population.h"
 
 namespace stride {
 namespace checkpoint {
@@ -20,12 +22,11 @@ public:
 	/// Constructor The string is the file for the checkpoints. The boolean is false if the file needs to be
 	/// created.
 	CheckPoint(std::string, bool create_mode = false);
-	
+
 	/// Loads a checkpoint from the file in the constructor. The unsigned int tells which checkpoint to use.
-	
 	void LoadCheckPoint(unsigned int);
+
 	/// Saves the current simulation to a checkpoint.
-	
 	void SaveCheckPoint();
 
 private:
@@ -37,15 +38,32 @@ private:
 
 	/// Writes the holidays.
 	void WriteHolidays(const boost::property_tree::ptree&);
-	
+
 	/// Loads the holidays into a boost::property_tree::ptree.
-	boost::property_tree::ptree LoadHolidays();
-	
+	std::unique_ptr<boost::property_tree::ptree> LoadHolidays();
+
+	/// Writes Contact Matrices.
+	void WriteMatrices();
+
+	/// Loads Contact Matrices.
+
+	/// Writes the disease.
+	void WriteDisease(const boost::property_tree::ptree&);
+
+	/// Loads the disease into a boost::property_tree::ptree.
+	std::unique_ptr<boost::property_tree::ptree> LoadDisease();
+
 	/// Writes the current date to the checkpoint.
 	void WriteDate(const Calendar&);
-	
-	/// Loads the current date from a checkpoint
+
+	/// Loads the current date from a checkpoint.
 	boost::gregorian::date LoadDate();
+
+	/// Writes the current population to a checkpoint.
+	void WritePopulation(const Population&);
+
+	/// Loads the asked population from the given checkpoint.
+	Population LoadPopulation(unsigned int);
 
 	hid_t m_file;
 };
