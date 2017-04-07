e9b2471cbe330b1fbfc319c8e1cd6a2be55e9787
jonathanvdc <jonathan.vdc@outlook.com>
2017-04-07 12:11:10 +0200

    Remove an unnecessary template declaration

--- src/main/cpp/pop/Population.h
+++ src/main/cpp/pop/Population.h
@@ -41,9 +41,6 @@ private:
 	std::map<PersonId, Person> people;
 	PersonId max_person_id;
 
-	template <typename TMapIterator>
-	class PopulationIterator;
-
 private:
 	/// An iterator implementation for Population containers.
 	template <typename TMapIterator>
