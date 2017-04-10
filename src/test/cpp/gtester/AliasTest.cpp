bb36dfadb1912482d2fa25475b5e3ef166ccc7fa
jonathanvdc <jonathan.vdc@outlook.com>
2017-04-10 15:06:27 +0200

    Create some biased RNG unit tests

--- src/test/cpp/gtester/AliasTest.cpp
+++ src/test/cpp/gtester/AliasTest.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 #include <alias/Alias.h>
 #include <alias/AliasUtil.h>
@@ -19,6 +21,51 @@ TEST(Alias, Next)
 {
 	stride::util::Random rng(0);
 	Alias::CreateDistribution({1.0}, rng).Next();
+}
+
+TEST(Alias, Generate)
+{
+	stride::util::Random rng(0);
+	std::vector<double> probabilities = {0.9, 0.05, 0.05};
+	auto distribution = Alias::CreateDistribution(probabilities, rng);
+	std::unordered_map<std::size_t, int> frequencies;
+	int sample_size = 10000;
+	for (int i = 0; i < sample_size; i++) {
+		frequencies[distribution.Next()]++;
+	}
+	for (const auto& pair : frequencies) {
+		ASSERT_NEAR(
+		    static_cast<double>(pair.second) / static_cast<double>(sample_size), probabilities[pair.first], 0.1)
+		    << "key: " << pair.first;
+	}
+}
+
+TEST(Alias, BiasedRandomValueGenerator)
+{
+	stride::util::Random rng(0);
+	enum Color
+	{
+		Red,
+		Green,
+		Blue
+	};
+	std::unordered_map<Color, double> probabilities;
+	probabilities[Red] = 0.9;
+	probabilities[Green] = 0.05;
+	probabilities[Blue] = 0.05;
+	auto random_val_generator = BiasedRandomValueGenerator<Color>::CreateDistribution(probabilities, rng);
+	std::unordered_map<Color, int> frequencies;
+	int sample_size = 10000;
+	for (int i = 0; i < sample_size; i++) {
+		frequencies[random_val_generator.Next()]++;
+	}
+	double cumulative_probability = probabilities[Red] + probabilities[Green] + probabilities[Blue];
+	for (const auto& pair : frequencies) {
+		ASSERT_NEAR(
+		    static_cast<double>(pair.second) / static_cast<double>(sample_size),
+		    probabilities[pair.first] / cumulative_probability, 0.1)
+		    << "color: " << pair.first;
+	}
 }
 
 } // Tests
