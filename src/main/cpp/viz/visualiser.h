4f85bd7c548ec06874928b283c1730c847c39ec2
sibert-aerts <sibert.aerts@gmail.com>
2017-04-18 13:36:51 +0200

    Remove Awesomium

---
+++ src/main/cpp/viz/visualiser.h
@@ -0,0 +1,18 @@
+#ifndef VISUALISER_H_INCLUDED
+#define VISUALISER_H_INCLUDED
+
+#include <unistd.h>
+#include <iostream>
+
+
+namespace Stride {
+
+class Visualiser{
+    public:
+    Visualiser();
+    void run();
+};
+
+}
+
+#endif  // end of include guard
