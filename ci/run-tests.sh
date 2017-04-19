21605da87b797cc8d8c09e991695f1ac638b8418
jonathanvdc <jonathan.vdc@outlook.com>
2017-04-19 12:59:52 +0200

    Inline 'run-tests.sh'

--- ci/run-tests.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -o pipefail
-
-# Run the tests.
-make test_all_no_x
-# Pretty-print the results.
-mono gtest-report-tools/gtest-report-print.exe build/installed/bin/gtester_all_no_x.xml
