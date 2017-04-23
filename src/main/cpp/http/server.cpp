e84028a8df6c0af0c498e707f6879f261aa0c51c
sibert-aerts <sibert.aerts@gmail.com>
2017-04-23 16:45:17 +0200

    Add StrideServer stub

--- src/main/cpp/http/server.cpp
+++ src/main/cpp/http/server.cpp
@@ -1,12 +1,57 @@
+#include "Poco/JSON/Object.h"
 #include "server.h"
 
 using namespace std;
 
 namespace Stride {
-namespace HTTP {
 
-Visualiser::Visualiser() {}
+// NotFoundRequestHandler
 
-void Visualiser::run() { return; }
+NotFoundRequestHandler::NotFoundRequestHandler() {}
+
+void NotFoundRequestHandler::handleRequest(HTTPServerRequest& request, HTTPServerResponse& response)
+{
+	response.setChunkedTransferEncoding(true);
+	response.setContentType("json");
+
+	Poco::JSON::Object out;
+	out.set("message", "Not found!");
+
+	std::ostream& ostr = response.send();
+	out.stringify(ostr);
+}
+
+// StrideRequestHandlerFactory
+
+StrideRequestHandlerFactory::StrideRequestHandlerFactory() {}
+
+HTTPRequestHandler* StrideRequestHandlerFactory::createRequestHandler(const HTTPServerRequest& request)
+{
+	string url = request.getURI();
+	if (url == "/API/")
+		return new NotFoundRequestHandler();
+	else
+		return nullptr;
+}
+
+// StrideServer
+
+StrideServer::StrideServer() {}
+StrideServer::~StrideServer() {}
+
+int StrideServer::run(unsigned short port)
+{
+	ServerSocket svs(port);
+	StrideRequestHandlerFactory* factory = new StrideRequestHandlerFactory();
+	HTTPServerParams* params = new HTTPServerParams;
+	HTTPServer srv(factory, svs, params);
+	// Start the server
+	srv.start();
+	// Wait until some call for termination occurs
+	waitForTerminationRequest();
+	// Stop the server
+	srv.stop();
+	delete factory;
+	return Application::EXIT_OK;
 }
 }
--- src/main/cpp/http/server.h
+++ src/main/cpp/http/server.h
@@ -3,17 +3,72 @@
 
 #include <iostream>
 #include <unistd.h>
+#include "Poco/DateTimeFormat.h"
+#include "Poco/DateTimeFormatter.h"
+#include "Poco/Exception.h"
+#include "Poco/Net/HTTPRequestHandler.h"
+#include "Poco/Net/HTTPRequestHandlerFactory.h"
+#include "Poco/Net/HTTPServer.h"
+#include "Poco/Net/HTTPServerParams.h"
+#include "Poco/Net/HTTPServerParams.h"
+#include "Poco/Net/HTTPServerRequest.h"
+#include "Poco/Net/HTTPServerResponse.h"
+#include "Poco/Net/ServerSocket.h"
+#include "Poco/Process.h"
+#include "Poco/ThreadPool.h"
+#include "Poco/Timestamp.h"
+#include "Poco/Util/HelpFormatter.h"
+#include "Poco/Util/Option.h"
+#include "Poco/Util/OptionSet.h"
+#include "Poco/Util/ServerApplication.h"
+
+using Poco::Net::ServerSocket;
+using Poco::Net::HTTPRequestHandler;
+using Poco::Net::HTTPRequestHandlerFactory;
+using Poco::Net::HTTPServer;
+using Poco::Net::HTTPServerRequest;
+using Poco::Net::HTTPServerResponse;
+using Poco::Net::HTTPServerParams;
+using Poco::Timestamp;
+using Poco::DateTimeFormatter;
+using Poco::DateTimeFormat;
+using Poco::ThreadPool;
+using Poco::Util::ServerApplication;
+using Poco::Util::Application;
+using Poco::Util::Option;
+using Poco::Util::OptionSet;
+using Poco::Util::OptionCallback;
+using Poco::Util::HelpFormatter;
 
 namespace Stride {
-namespace HTTP {
 
-class Visualiser
+class NotFoundRequestHandler : public HTTPRequestHandler
 {
 public:
-	Visualiser();
-	void run();
+	NotFoundRequestHandler();
+	void handleRequest(HTTPServerRequest& request, HTTPServerResponse& response);
 };
-}
-}
+
+class StrideRequestHandlerFactory : public HTTPRequestHandlerFactory
+{
+public:
+	StrideRequestHandlerFactory();
+	HTTPRequestHandler* createRequestHandler(const HTTPServerRequest& request);
+
+private:
+	// Put our window into the simulator here
+};
+
+class StrideServer : public Poco::Util::ServerApplication
+{
+public:
+	StrideServer();
+	~StrideServer();
+
+	// Run the Stride Server at localhost:`port`
+	int run(unsigned short port);
+};
+
+} // namespace Stride
 
 #endif // end of include guard
