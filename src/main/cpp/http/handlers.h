4182156b4be42f9fa37a538137ad30c92cd2a50b
sibert-aerts <sibert.aerts@gmail.com>
2017-04-23 18:54:23 +0200

    Steps towards URL structure

--- src/main/cpp/http/handlers.h
+++ src/main/cpp/http/handlers.h
@@ -4,12 +4,12 @@
 #include <iostream>
 #include <unistd.h>
 #include "Poco/Exception.h"
+#include "Poco/JSON/Object.h"
 #include "Poco/Net/HTTPRequestHandler.h"
 #include "Poco/Net/HTTPServer.h"
 #include "Poco/Net/HTTPServerParams.h"
 #include "Poco/Net/HTTPServerRequest.h"
 #include "Poco/Net/HTTPServerResponse.h"
-#include "Poco/JSON/Object.h"
 
 using Poco::Net::HTTPRequestHandler;
 using Poco::Net::HTTPServerRequest;
@@ -21,6 +21,13 @@ class NotFoundRequestHandler : public HTTPRequestHandler
 {
 public:
 	NotFoundRequestHandler();
+	void handleRequest(HTTPServerRequest& request, HTTPServerResponse& response);
+};
+
+class DataRequestHandler : public HTTPRequestHandler
+{
+public:
+	DataRequestHandler();
 	void handleRequest(HTTPServerRequest& request, HTTPServerResponse& response);
 };
 }
