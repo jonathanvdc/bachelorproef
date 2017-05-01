1757d3736fc1959e9aaaaec6f71234f736ac9277
jonathanvdc <jonathan.vdc@outlook.com>
2017-05-01 13:16:10 +0200

    Abstract over #pragma omp parallel for

--- src/main/cpp/util/Parallel.h
+++ src/main/cpp/util/Parallel.h
@@ -6,11 +6,13 @@
  * A paper-thin abstraction layer over parallelization technologies.
  */
 
+#include <vector>
 #include <omp.h>
 
 namespace stride {
 namespace util {
 namespace parallel {
+
 /// Gets the number of threads that are available for parallelization.
 inline unsigned int get_number_of_threads()
 {
@@ -20,6 +22,17 @@ inline unsigned int get_number_of_threads()
 		num_threads = omp_get_num_threads();
 	}
 	return num_threads;
+}
+
+/// Applies the given action to the given list of values.
+template <typename T, typename TAction>
+inline void parallel_for(std::vector<T>& values, unsigned int num_threads, const TAction& action)
+{
+#pragma omp parallel for num_threads(num_threads) schedule(runtime)
+	for (size_t i = 0; i < values.size(); i++) {
+		const unsigned int thread_id = omp_get_thread_num();
+		action(values[i], thread_id);
+	}
 }
 }
 }
